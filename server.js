3deec7a0c4b1d7a5f97a8ca7931578e53ab1c0c8
coolchaeyoung <ggodchae@naver.com>
2021-03-30 21:42:27 +0900

    #3.6 Prisma Client

--- server.js
+++ server.js
@@ -1,21 +1,40 @@
+import { PrismaClient } from ".prisma/client";
 import { ApolloServer, gql } from "apollo-server";
+
+const client = new PrismaClient();
 
 const typeDefs = gql`
   type Movie {
-    id: Int
-    title: String
-    year: Int
+    id: Int!
+    title: String!
+    year: Int!
+    genre: String
+    createdAt: String!
+    updatedAt: String!
   }
   type Query {
     movies: [Movie]
     movie: Movie
   }
+  type Mutation {
+    createMovie(title: String!, year: Int, genre: String): Movie
+  }
 `;
 
 const resolvers = {
   Query: {
-    movies: () => [],
+    movies: () => client.movie.findMany(),
     movie: () => ({ title: "Hello", year: 2021 }),
+  },
+  Mutation: {
+    createMovie: (_, { title, year, genre }) =>
+      client.movie.create({
+        data: {
+          title,
+          year,
+          genre,
+        },
+      }),
   },
 };
 
