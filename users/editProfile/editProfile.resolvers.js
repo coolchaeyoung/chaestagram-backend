3858035aa40e54e119542c0af877da038d727634
coolchaeyoung <ggodchae@naver.com>
2021-04-06 22:49:30 +0900

    #4.7 updateProfile

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,7 +1,30 @@
+import bcrypt from "bcrypt";
+import client from "../../client";
+
 export default {
   Mutation: {
-    editProfile: (_, { irstName, lastName, username, email, password }) => {
-      console.log("hi");
+    editProfile: async (
+      _,
+      { firstName, lastName, username, email, password }
+    ) => {
+      let uglyPassword = null;
+      if (password) {
+        uglyPassword = await bcrypt.hash(password, 10);
+      }
+      const updatedUser = await client.user.update({
+        where: { id: 1 },
+        data: {
+          firstName,
+          lastName,
+          username,
+          email,
+          ...(uglyPassword && { password: uglyPassword }),
+        },
+      });
+      if (updatedUser.id) {
+        return { ok: true };
+      }
+      return { ok: false, error: "Could not update Profile" };
     },
   },
 };
