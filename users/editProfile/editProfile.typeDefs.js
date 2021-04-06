96b5fecdb43d2b570587713fb5f61ce1f88336d9
coolchaeyoung <ggodchae@naver.com>
2021-04-06 20:07:10 +0900

    #4.6 Divide and Conquer

--- users/editProfile/editProfile.typeDefs.js
+++ users/editProfile/editProfile.typeDefs.js
@@ -1,15 +1,17 @@
 import { gql } from "apollo-server";
 
-export default gql`{
-    type EditProfileResult {
+export default gql`
+  type EditProfileResult {
     ok: Boolean!
     error: String
   }
-    type Mutation {
-        editProfile(firstName: String
-            lastName: String
-            username: String
-            email: String
-            password: String): EditProfileResult!
-    }
-}`;
+  type Mutation {
+    editProfile(
+      firstName: String
+      lastName: String
+      username: String
+      email: String
+      password: String
+    ): EditProfileResult!
+  }
+`;
