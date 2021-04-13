5bfbca1c37be2bb59603a03f69b61dd031b00870
coolchaeyoung <ggodchae@naver.com>
2021-04-14 00:29:25 +0900

    #4.18 Ejecting from Apollo Server

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
 require("dotenv").config();
-import { ApolloServer } from "apollo-server";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
 import { typeDefs, resolvers } from "./schema";
-import { getUser, protectResolver } from "./users/users.utils";
+import { getUser } from "./users/users.utils";
+import morgan from "morgan";
 
+const PORT = process.env.PORT;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: async ({ req }) => {
     return {
       loggedInUser: await getUser(req.headers.token),
-      protectResolver,
     };
   },
 });
 
-const PORT = process.env.PORT;
-
-server
-  .listen(PORT)
-  .then(() => console.log(`Server is running on http://localhost:${PORT}/`));
+const app = express();
+app.use(morgan("tiny");
+server.applyMiddleware({ app });
+app.listen({ port: 4000 }, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
